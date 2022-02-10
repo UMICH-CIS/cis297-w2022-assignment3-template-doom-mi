6feb9d529489c461b9ea4f3881aafbf5e297ea73
Dominic <dominicver@hotmail.com>
2022-02-10 00:58:15 -0500

    Downgrade in framework from 4.7.1 to 4.6.1 in order to work on project as well as added in classes

---
+++ BankApplication/Account.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApplication
+{
+    public class Account
+    {
+        private decimal accountBalance;
+
+        public decimal Balance { get { return accountBalance; } }
+
+        public Account(decimal initialBalance)
+        {
+            accountBalance = initialBalance;
+            if(accountBalance <= 0)
+            {
+                throw new ArgumentException(paramName: nameof(accountBalance), message: "Account balance cannot be at a zero/negative balance.");
+            }
+
+        }
+
+        public void Credit(decimal money)
+        {
+            accountBalance += money;
+        }
+
+        public void Debit(decimal money)
+        {
+            if(accountBalance - money < 0)
+            {
+                Console.WriteLine("Debit amount exceeded account balance.");
+            }
+
+            else
+            {
+                accountBalance -= money;
+            }
+        }
+    }
+}
